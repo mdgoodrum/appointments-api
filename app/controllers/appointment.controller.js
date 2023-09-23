a998d07b2cfc1ba559a26aa45e6258b50b02dbd8
Michael Goodrum <michaeldgoodrum@gmail.com>
2023-09-23 11:18:16 -0300

    add appointment controller v1

---
+++ app/controllers/appointment.controller.js
@@ -0,0 +1,57 @@
+const db = require('../models');
+
+const Appointment = db.appointments;
+
+exports.create = (req, res) => {
+  if (!req.body.provider) {
+    res.status(400).send({
+      message: 'Provider can not be empty!',
+    });
+    return;
+  }
+
+  if (!req.body.start) {
+    res.status(400).send({
+      message: 'Start can not be empty!',
+    });
+    return;
+  }
+
+  if (!req.body.end) {
+    res.status(400).send({
+      message: 'End can not be empty!',
+    });
+    return;
+  }
+
+  const appointment = {
+    provider: req.body.provider,
+    timeslot_start: req.body.start,
+    timeslot_end: req.body.end,
+    status: 'available',
+  };
+
+  Appointment.create(appointment)
+    .then((data) => {
+      res.send(data);
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message:
+          err.message || 'Some error occurred while creating the Appointment.',
+      });
+    });
+};
+
+exports.findAll = (req, res) => {
+  Appointment.findAll({ where: { status: 'available' } })
+    .then((data) => {
+      res.send(data);
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message:
+            err.message || 'Some error occurred while retrieving appointments.',
+      });
+    });
+};
